d5efdc95c9f52db5f8d08d4dcc3a5dccd43cede4
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-03-18 14:12:03 +0530

    design fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import { BrowserRouter, Redirect, Switch, Route } from 'react-router-dom';
-import NotFound from './NotFound';
-import routes from './Routes';
-import RouteWithSubRoutes from './utils/RouteWithSubRoutes';
-import Home from './Home';
-import Login from './Login';
-import Page1 from './pages/Page1';
-import Page2 from './pages/Page2';
-import Subpage1 from './pages/subpages/Subpage1';
-import Subpage2 from './pages/subpages/Subpage2';
+import { BrowserRouter, Redirect, Switch, Route } from "react-router-dom";
+import NotFound from "./NotFound";
+import routes from "./Routes";
+import RouteWithSubRoutes from "./utils/RouteWithSubRoutes";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Switch>
-				<Redirect exact from='/' to='/login' />
-				{routes.map((route, i) => (
-					<RouteWithSubRoutes key={i} {...route} />
-				))}
-				<Route component={NotFound} />
-			</Switch>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Redirect exact from="/" to="/login" />
+        {routes.map((route, i) => (
+          <RouteWithSubRoutes key={i} {...route} />
+        ))}
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
