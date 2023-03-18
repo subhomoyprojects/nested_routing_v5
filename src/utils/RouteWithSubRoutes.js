33e915f502f6c65952723b3d6e5838630b644598
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-03-18 14:15:53 +0530

    design fix

--- src/utils/RouteWithSubRoutes.js
+++ src/utils/RouteWithSubRoutes.js
@@ -1,26 +1,12 @@
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 
 const RouteWithSubRoutes = (route) => {
-	return (
-		<Route
-			path={route.path}
-			render={(props) => (
-				<route.component {...props} routes={route.routes} />
-			)}
-		/>
-	);
+  return (
+    <Route
+      path={route.path}
+      render={(props) => <route.component {...props} routes={route.routes} />}
+    />
+  );
 };
 
 export default RouteWithSubRoutes;
-
-
-{/* <Route path="/home" render={(props)=><Home {...props} routes={[
-	{
-		path: '/home/page1/subpage1',
-		component: Subpage1,
-	},
-	{
-		path: '/home/page1/subpage2',
-		component: Subpage2,
-	},
-]}/>} */}
